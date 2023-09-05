28029c31cacbae44ee0f375059c98ea8e66e587e
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-09-05 14:47:43 +0200

    create checkout session

--- server/resources/checkout/checkout.controller.js
+++ server/resources/checkout/checkout.controller.js
@@ -1,22 +1,24 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const CLIENT_URL = 'http://localhost:5173'
 
 const createCheckOutSession = async (req,res) => {
     try {
         const session = await stripe.checkout.sessions.create({
             line_items: [
-                {price: 'price_1NmxufDbqBxej6tjFCbI8GcO', quantity: 10},
-                {price: 'price_1NmxqfDbqBxej6tjzKTHgBlx', quantity: 10},
-                {price: 'price_1Nmxp3DbqBxej6tjr5dLaeWb', quantity: 10},
-                {price: 'price_1NmxcIDbqBxej6tjDoRLPLRo', quantity: 10},
+                {price: 'price_1NmxufDbqBxej6tjFCbI8GcO', quantity: 2},
+                {price: 'price_1NmxqfDbqBxej6tjzKTHgBlx', quantity: 2},
             ],
             mode: 'payment',
             success_url: `${CLIENT_URL}/confirmation`,
             cancel_url: CLIENT_URL, //Avbryter betalningen
         })
 
+        res.status(200).json({url: session.url})
+
     } catch (error) {
         console.log(error.message)
+        res.status(400).json('Det gick inte bra...')
     }
-
 }
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const PORT = 3000
 const app = express()
-
-const CLIENT_URL = 'http://localhost:5173'
 
 //Middlewares
 app.use(cors({
