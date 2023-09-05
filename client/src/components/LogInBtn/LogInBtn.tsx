cc0f2575d3fece82d6992b1dd3f239b6993d5c1e
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-09-05 17:43:41 +0200

    Log In Drawer

--- client/src/components/LogInBtn/LogInBtn.tsx
+++ client/src/components/LogInBtn/LogInBtn.tsx
@@ -1,11 +1,29 @@
-import { Button } from 'antd';
+import { Button, Drawer } from 'antd';
+import { useState } from 'react'
 
 function LogInBtn() {
+
+  const [open, setOpen] = useState(false);
+
+  const showLogInDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-    <Button type="primary">Logga in</Button>
+      <Button type="primary" onClick={showLogInDrawer}>
+        Logga In
+      </Button>
+      <Drawer title="Logga in eller Bli medlem" placement="right" onClose={onClose} open={open}>
+        <p>Logga In Här</p>
+        <p>Bli Medlem Här</p>
+      </Drawer>
     </>
-  )
-}
+  );
+};
 
 export default LogInBtn
