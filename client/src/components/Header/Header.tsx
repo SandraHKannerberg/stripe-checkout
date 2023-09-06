a89df07b41509807d90a00ee146f1ee2fdac0845
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-09-06 08:35:08 +0200

    update header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -5,7 +5,10 @@ import Logotype from '../Logotype/Logotype';
 function Header() {
   return (
     <div className='header--container'>
-        <Row justify="space-around" align="bottom" style={{'height':'25%', 'borderBottom' : '2px solid black'}}>
+        <Row justify="center" align="middle" style={{'backgroundColor' : 'black', 'height' : '1.8rem'}}>
+          <Col span={12}><p style={{'color':'white'}}>REKLAM / KAMPANJKOD HÄR</p></Col>
+        </Row>
+        <Row justify="space-around" align="bottom" style={{'height':'6rem', 'borderBottom' : '2px solid black'}}>
             <Col span={12}><Logotype></Logotype></Col>
             <Col span={12}><NavBar></NavBar></Col>
         </Row>
