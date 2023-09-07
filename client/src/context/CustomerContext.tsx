f006a4b750e2368e27292cc47c5597d2c31080be
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-09-07 14:50:35 +0200

    create CustomerContext

---
+++ client/src/context/CustomerContext.tsx
@@ -0,0 +1,99 @@
+import { createContext, useState, useEffect, useContext, PropsWithChildren } from "react";
+
+export interface Customer {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type CustomerType = {
+  username: string;
+  password: string;
+};
+
+interface ICustomerContext {
+  loggedInCustomer?: Customer | null;
+  login: (customer: CustomerType) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+const defaultValues = {
+    loggedInCustomer: null,
+    login: async () => {},
+    logout: async () => {},
+}
+
+export const CustomerContext = createContext<ICustomerContext>(defaultValues);
+
+export const useCustomerContext = () => useContext(CustomerContext);
+
+
+export const CustomerProvider = ({ children }: PropsWithChildren<{}>) => {
+
+  const [loggedInCustomer, setloggedInCustomer] = useState<Customer | null>(null);
+
+  useEffect(() => {
+    const authorization = async () => {
+      try {
+        const response = await fetch("/api/customers/authorize");
+        const data = await response.json();
+        if (response.status === 200 || response.status === 304) {
+          setloggedInCustomer(data);
+        }
+ 
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    authorization();
+  }, []);
+
+  const login = async (customer : CustomerType) => {
+    if (customer) {
+      try {
+        const response = await fetch("api/customers/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(customer),
+        });
+        const data = await response.json();
+ 
+        if (response.status === 200) {
+          setloggedInCustomer(data);
+        } 
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const logout = async () => {
+
+    try {
+      const response = await fetch("api/customers/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 204) {
+        setloggedInCustomer(null);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <CustomerContext.Provider
+      value={{ loggedInCustomer, login: login, logout: logout }}
+    >
+      {children}
+    </CustomerContext.Provider>
+  );
+};
+
+export default CustomerProvider;
