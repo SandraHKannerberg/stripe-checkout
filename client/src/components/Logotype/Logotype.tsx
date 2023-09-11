887d9f6fb5777acc4614e78ae9211e5ae1bb95da
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-09-11 22:40:24 +0200

    logotype

--- client/src/components/Logotype/Logotype.tsx
+++ client/src/components/Logotype/Logotype.tsx
@@ -3,9 +3,8 @@ import { Space } from "antd"
 function Logotype() {
   return (
 
-    <Space style={{"marginLeft":"2rem"}}>
-      Sunflower Poster
-      <img src="/client/assets/" alt="Sunflower Posters Logotype" />
+    <Space style={{"marginLeft":"3rem", "height":"6rem", "display": "flex", "alignItems":"center", "justifyContent":"flex-start"}}>
+      <img src="/assets/logotype.png" alt="Sunflower Posters Logotype" height="90rem"/>
     </Space>
   )
 }
