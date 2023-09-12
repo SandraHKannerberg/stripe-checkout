30b7dda42841dc5db9d41d43f2dae77d4c4d008d
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-09-12 13:38:27 +0200

    basic layout for confirmation page - checkout success

--- client/src/pages/Confirmation.tsx
+++ client/src/pages/Confirmation.tsx
@@ -1,13 +1,20 @@
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 import { Col, Row } from "antd";
+import { NavLink } from "react-router-dom";
 
 function confirmation() {
   return (
     <Row style={{"minHeight" : "100vh", "backgroundColor":"whitesmoke"}} >
         <Col span={24}><Header></Header></Col>
-        <Col style={{"minHeight" : "70vh"}} span={24}>
-            Tack för ditt köp!
+        <Col style={{"minHeight" : "70vh", "textAlign":"center"}} span={24}>
+            
+           <h3>Tack för ditt köp!</h3>
+
+           <NavLink to="/"style={{ textDecoration: "none" }}>
+                <p>TILLBAKA TILL BUTIKEN</p>
+            </NavLink>
+
         </Col>
         <Col span={24}><Footer></Footer></Col>
     </Row>
