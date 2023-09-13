b2541d3972d922b8e8b0f8d95b82bcce0a73c8d0
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-09-13 12:12:00 +0200

    coupon layout in header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,19 +1,20 @@
 import { Col, Row } from "antd";
+import Coupon from "../CheckoutBtn/Coupon/Coupon";
 import NavBar from "../NavBar/NavBar";
 import Logotype from "../Logotype/Logotype";
 import "./Header.css"
-import { GiftOutlined } from '@ant-design/icons';
+
 
 function Header() {
   return (
     <div className="header--container">
-        <Row justify="center" align="middle" style={{"backgroundColor" : "black", "height" : "1.8rem"}}>
-          <Col span={24}><p style={{"color":"white", "textAlign":"center"}}><GiftOutlined /> AKTUELL KAMPANJKOD: </p></Col>
-        </Row>
-        <Row justify="space-around" align="bottom" style={{"height":"6rem"}}>
-            <Col span={12}><Logotype></Logotype></Col>
-            <Col span={12}><NavBar></NavBar></Col>
-        </Row>
+
+      <Coupon></Coupon>
+    
+      <Row justify="space-around" align="bottom" style={{"height":"6rem"}}>
+        <Col span={12}><Logotype></Logotype></Col>
+        <Col span={12}><NavBar></NavBar></Col>
+      </Row>
     </div>
   )
 }
