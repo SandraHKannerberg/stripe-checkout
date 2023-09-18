e125e1d2838a9998b4db74a51f2f56a218e02aa9
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-09-18 15:48:01 +0200

    logotype - add link to start

--- client/src/components/Logotype/Logotype.tsx
+++ client/src/components/Logotype/Logotype.tsx
@@ -1,11 +1,14 @@
 import { Space } from "antd"
+import { NavLink } from "react-router-dom"
 
 function Logotype() {
   return (
 
+  <NavLink className="homeicon" to="/"style={{ textDecoration: "none"}}>
     <Space style={{"marginLeft":"3rem", "height":"6rem", "display": "flex", "alignItems":"center", "justifyContent":"flex-start"}}>
       <img src="/assets/logotype-sunflower.png" alt="Sunflower Posters Logotype" height="80rem"/>
     </Space>
+  </NavLink>
   )
 }
 
