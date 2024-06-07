f4b86df1e97ed09b50abe1b1be2c9754ac0377ce
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-06-07 09:55:38 +0200

    Update import logotype

--- client/src/components/Logotype/Logotype.tsx
+++ client/src/components/Logotype/Logotype.tsx
@@ -1,12 +1,13 @@
 import { Col, Space } from "antd"
 import { NavLink } from "react-router-dom"
+import logo from '../../assets/logotype-sunflower.png'
 
 function Logotype() {
   return (
   <Col span={24} style={{maxHeight: "6rem"}}>
     <NavLink className="homeicon" to="/"style={{ textDecoration: "none"}}>
       <Space style={{marginLeft:"1.5rem", minHeight:"3rem"}}>
-        <img src="/assets/logotype-sunflower.png" alt="Sunflower Posters Logotype" height="80rem"/>
+        <img src={logo} alt="Sunflower Posters Logotype" height="80rem"/>
       </Space>
     </NavLink>
   </Col>
