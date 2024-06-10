ae812c4f2b115e36fbba776a574a167e2b983c77
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-06-10 10:36:01 +0200

    .

--- server/app.js
+++ server/app.js
@@ -14,30 +14,29 @@ const app = express()
 //     origin: '*'
 // }))
 
-// const corsOptions = {
-//   origin: 'https://stripe-checkout-project.onrender.com/', // Frontend URL
-//   // origin: 'http://localhost:5173',
-//   methods: 'GET,POST,PUT,DELETE',
-//   allowedHeaders: 'Content-Type,Authorization',
-//   credentials: true
-// };
+const corsOptions = {
+  origin: 'https://checkout-sandra.netlify.app/', // Frontend URL
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://checkout-sandra.netlify.app/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET; POST; PUT; DELETE; OPTIONS"
-  )
-})
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://checkout-sandra.netlify.app/"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET; POST; PUT; DELETE; OPTIONS"
+//   )
+// })
 
 const secretKey = process.env.COOKIE_SECRET_KEY;
 
